30ed14cc77d18661c8e26636099b8a463ef86c99
elmirahtm <elmirahatamova28@gmail.com>
2023-11-17 21:12:12 +0400

    massiv2

---
+++ massiv2/app.js
@@ -0,0 +1,6 @@
+// . Adlar massivini parametr kimi qebul eden bir funksiya yazin, bele ki, o funksiyadan bize en uzun adi cixaracaq. 
+// mes: // let arr = ['ali', 'reza', 'alireza', 'muhammad', 'coshqun', 'eles'] 
+// findLongestName(arr); // 'muhammad'
+let ad=["gunel","ayan","gulcin","gultekin"];
+let longthname=ad.find((ad)=>ad.length >6)
+console.log(longthname);
