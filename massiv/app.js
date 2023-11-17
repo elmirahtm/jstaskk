ee4d9b409c51a4ba18ced0b666174c8228b93edd
elmirahtm <elmirahatamova28@gmail.com>
2023-11-17 20:21:21 +0400

    massiv

---
+++ massiv/app.js
@@ -0,0 +1,19 @@
+
+// 1. Rəqəmlər massivini parametr kimi qəbul edən 
+// və həmin massivdən konsola bütün mənfi ədədləri çıxaran funksiya yazın.
+
+
+let numbers= [5,6,-8,93,4,6,10,-4,-6,-52]
+let negativnumber=numbers.filter((numbers)=>numbers<0)
+console.log(negativnumber);
+
+
+
+
+// const negativnumber=(numbers)=>{
+//      if (numbers<0) {
+     
+//      }
+       
+// }
+// negativnumber(numbers)
